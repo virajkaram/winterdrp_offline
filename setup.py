04614b3b417d4f0a7e06744ea506dedf0519b2e6
Nathan Lourie <nlourie@mit.edu>
2025-03-17 16:05:32 -0400

    working on playing with lab flats

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 import setuptools
-from pathlib import Path
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='winterdrp_offline',
-    version='0.0.1',
-    author='Viraj Karambelkar',
-    requires=['astropy', 'numpy', 'scipy', 'matplotlib',
-              'astroquery', 'pathlib', 'os', 'glob', 'matplotlib',
-              ],
+    name="winterdrp_offline",
+    version="0.0.1",
+    author="Viraj Karambelkar",
+    install_requires=[
+        "astropy",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "astroquery",
+    ],
+    # Let setuptools find your package automatically from the current directory
+    packages=setuptools.find_packages(),
+    # Possibly also:
+    python_requires=">=3.7",
 )
-
--- winterdrp_offline/utils.py
+++ winterdrp_offline/utils.py
@@ -1,11 +1,13 @@
 import os
 from glob import glob
-from astropy.stats import sigma_clipped_stats
-import matplotlib.pyplot as plt
-from astropy.io import fits
-import numpy as np
-from astropy.table import Table
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
+from astropy.table import Table
+from astropy.visualization import ImageNormalize, SqrtStretch, ZScaleInterval
 
 
 def subtract_dark(image: np.ndarray, masterdark: np.ndarray):
@@ -58,7 +60,6 @@ def write_weight_images(imagelist: str | Path | list):
     return masklist
 
 
-
 def convert_hdu_to_ldac(hdu):
     """
     Convert an hdu table to a fits_ldac table (format used by astromatic suite)
@@ -75,16 +76,17 @@ def convert_hdu_to_ldac(hdu):
     tbl2: `astropy.io.fits.BinTableHDU`
         Data table (LDAC_OBJECTS)
     """
-    from astropy.io import fits
     import numpy as np
-    tblhdr = np.array([hdu.header.tostring(',')])
-    col1 = fits.Column(name='Field Header Card', array=tblhdr, format='13200A')
+    from astropy.io import fits
+
+    tblhdr = np.array([hdu.header.tostring(",")])
+    col1 = fits.Column(name="Field Header Card", array=tblhdr, format="13200A")
     cols = fits.ColDefs([col1])
     tbl1 = fits.BinTableHDU.from_columns(cols)
-    tbl1.header['TDIM1'] = '(80, {0})'.format(len(hdu.header))
-    tbl1.header['EXTNAME'] = 'LDAC_IMHEAD'
+    tbl1.header["TDIM1"] = "(80, {0})".format(len(hdu.header))
+    tbl1.header["EXTNAME"] = "LDAC_IMHEAD"
     tbl2 = fits.BinTableHDU(hdu.data)
-    tbl2.header['EXTNAME'] = 'LDAC_OBJECTS'
+    tbl2.header["EXTNAME"] = "LDAC_OBJECTS"
     return (tbl1, tbl2)
 
 
@@ -101,12 +103,14 @@ def convert_table_to_ldac(tbl):
     hdulist: `astropy.io.fits.HDUList`
         FITS_LDAC hdulist that can be read by astromatic software
     """
-    from astropy.io import fits
     import tempfile
-    f = tempfile.NamedTemporaryFile(suffix='.fits', mode='rb+')
-    tbl.write(f, format='fits')
+
+    from astropy.io import fits
+
+    f = tempfile.NamedTemporaryFile(suffix=".fits", mode="rb+")
+    tbl.write(f, format="fits")
     f.seek(0)
-    hdulist = fits.open(f, mode='update')
+    hdulist = fits.open(f, mode="update")
     tbl1, tbl2 = convert_hdu_to_ldac(hdulist[1])
     new_hdulist = [hdulist[0], tbl1, tbl2]
     new_hdulist = fits.HDUList(new_hdulist)
@@ -130,22 +134,106 @@ def save_table_as_ldac(tbl, filename, **kwargs):
     hdulist.writeto(filename, **kwargs)
 
 
-def plot_image(data, thresh=3.0, ax=None):
+def plot_image(
+    data,
+    thresh=3.0,
+    ax=None,
+    x_min=None,
+    x_max=None,
+    y_min=None,
+    y_max=None,
+    norm=None,
+    return_norm=False,
+):
     """
-    Plot an image with plt.imshow and threshold
-    :param data:
-    :param thresh:
-    :return:
+    Plot an image with plt.imshow, auto-thresholded via sigma_clipped_stats,
+    and optionally restrict to a window defined by (x_min:x_max, y_min:y_max).
+
+    Parameters
+    ----------
+    data : 2D np.ndarray
+        The input image to plot.
+    thresh : float, optional
+        The sigma threshold to use in sigma_clipped_stats. Default is 3.0.
+    ax : matplotlib.axes.Axes, optional
+        The axes on which to plot. If None, a new figure and axes will be created.
+    x_min : int, optional
+        Minimum x index (column) for the window. If None, defaults to 0.
+    x_max : int, optional
+        Maximum x index (column) for the window (non-inclusive). If None, defaults to data.shape[1].
+    y_min : int, optional
+        Minimum y index (row) for the window. If None, defaults to 0.
+    y_max : int, optional
+        Maximum y index (row) for the window (non-inclusive). If None, defaults to data.shape[0].
+    norm : matplotlib.colors.Normalize, optional
+        A normalization object to pass to imshow. If None, defaults to a linear normalization.
+    return_norm : bool, optional
+        If True, return the normalization object. Default is False
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The axes on which the image was plotted.
+    norm : astropy.visualization.ImageNormalize, optional
+        The normalization object used for the plot. Only returned if return_norm is True
     """
+    # Create ax if not provided
     if ax is None:
         fig, ax = plt.subplots()
 
-    _, med, std = sigma_clipped_stats(data, sigma=thresh)
-    ax.imshow(data, vmin=med-3*std, vmax=med+3*std, cmap='gray', origin='lower')
-    # plot colorbar
-    cbar = plt.colorbar(ax.imshow(data, vmin=med-3*std, vmax=med+3*std, cmap='gray', origin='lower'), ax=ax)
+    # Handle default window boundaries
+    if x_min is None:
+        x_min = 0
+    if x_max is None:
+        x_max = data.shape[1]
+    if y_min is None:
+        y_min = 0
+    if y_max is None:
+        y_max = data.shape[0]
 
-    return ax
+    # Slice the data to the desired window
+    windowed_data = data[y_min:y_max, x_min:x_max]
+
+    # Compute stats on the windowed region
+    _, med, std = sigma_clipped_stats(windowed_data, sigma=thresh)
+
+    # Create a normalization object if not provided
+    if norm is None:
+        # Plot
+        im = ax.imshow(
+            windowed_data,
+            vmin=med - 3 * std,
+            vmax=med + 3 * std,
+            cmap="gray",
+            origin="lower",
+        )
+    else:
+        if norm == "zscale":
+            norm = ImageNormalize(
+                windowed_data,
+                interval=ZScaleInterval(),
+                stretch=SqrtStretch(),
+            )
+        elif norm == "minmax":
+            norm = ImageNormalize(
+                windowed_data,
+                vmin=np.nanmin(windowed_data),
+                vmax=np.nanmax(windowed_data),
+            )
+        # Plot
+        im = ax.imshow(
+            windowed_data,
+            cmap="gray",
+            origin="lower",
+            norm=norm,
+        )
+
+    cbar = plt.colorbar(im, ax=ax)
+
+    # Return the normalization object if requested
+    if return_norm:
+        return ax, norm
+    else:
+        return ax
 
 
 def write_image(image, filename, header=None, overwrite=True):
